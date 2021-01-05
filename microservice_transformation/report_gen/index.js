59c0e106b179af7b33a40c0236a5417d29856c29
George Niece <george.niece@gmail.com>
2021-01-05 02:18:06 -0600

    Create Last 10 Report Output from MySQL

--- microservice_transformation/report_gen/index.js
+++ microservice_transformation/report_gen/index.js
@@ -1,44 +1,68 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
-
-app.get("/", async (req, res) => {
-    res.send({msg:'Hello George'});
+const port = process.env.PORT || 3333;
+/* modify the host for mysql connection from `minikube ip` output */
+var mysql = require('mysql');
+var con = mysql.createConnection({
+                                                          host: "172.17.0.57",
+                                                          user: "root",
+                                                          password: "password",
+                                                          database: "fortunedb"
 });
 
-const appName = 'Report Generator'
+con.connect(function(err) {
+                                                          if (err) throw err;
+                                                          console.log("Connected!");
+});
+
+app.get("/", async (req, res) => {
+                                      var sql = "select fortune_sender, fortune from fortunes order by fortune_id desclimit 10";
+                              var response = "<html><head><title>Fortune Report</title></head><body><h4><b>";
+                                      con.query(sql, function (err, result) {
+                                                          if (err) throw err;
+                                                            Object.keys(result).forEach(function(key) {
+                                                                            var row = result[key];
+                                                                            console.log(row.fortune_sender + " " + row.fortune)
+                                                                           response += row.fortune_sender + ": " + row.fortune + "</br>";
+                                                                          });
+                    response += "</body></html>";
+                    res.send(response);
+                                              });
+
+
+});
 
 app.use(express.json());
 
 const server = app.listen(port, () => {
- console.log(`Report Gen Server running on port:  ${port} at ${new Date()} on process: ${process.pid}`);
+                 console.log(`Report Gen Server running on port:  ${port} at ${new Date()} on process: ${process.pid}`);
 });
 
 const shutdown = async (signal) => {
-    let shutdownMessage;
+                    let shutdownMessage;
 
-    if(!signal){
-        shutdownMessage = (`${appName} API Server shutting down at ${new Date()}`);
-    }else{
-        shutdownMessage = (`Signal ${signal} : ${restaurant} API Server shutting down at ${new Date()}`);
-    }
-    const obj = {status:'SHUTDOWN', shutdownMessage, pid:process.pid};
-    await server.close(function () {
-        console.log(obj);
-        process.exit(0);
-    }).catch(err => {
-        console.error(err);
-        return {status:'ERROR',err}
-    })
+                    if(!signal){
+                                            shutdownMessage = (`${appName} API Server shutting down at ${new Date()}`);
+                                        }else{
+                                                                shutdownMessage = (`Signal ${signal} : API Server shutting down at ${new Date()}`);
+                                                            }
+                    const obj = {status:'SHUTDOWN', shutdownMessage, pid:process.pid};
+                    await server.close(function () {
+                                            console.log(obj);
+                                            process.exit(0);
+                                        }).catch(err => {
+                                                                console.error(err);
+                                                                return {status:'ERROR',err}
+                                                            })
 
 };
 
 process.on('SIGTERM', function () {
-    shutdown('SIGTERM');
+                    shutdown('SIGTERM');
 });
 
 process.on('SIGINT', function () {
-    shutdown('SIGINT');
+                    shutdown('SIGINT');
 });
 
 
