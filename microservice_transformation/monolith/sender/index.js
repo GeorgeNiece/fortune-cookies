a6965209db196147d517666c602806746f3e716c
George Niece <george.niece@gmail.com>
2021-01-05 02:21:35 -0600

    Update Sender for MySQL/redis
    Update Sender to serialize in MySQL and emit keyed data to Redis

--- microservice_transformation/monolith/sender/index.js
+++ microservice_transformation/monolith/sender/index.js
@@ -6,11 +6,34 @@ config = {
 }
  */
 
+var redis = require('redis');
+var mysql = require('mysql');
+var SqlString = require('sqlstring');
+var con = mysql.createConnection({
+                                          host: "172.17.0.57",
+                                          user: "root",
+                                          password: "password",
+                                          database: "fortunedb"
+});
 
+var client = redis.createClient("6379", "172.17.0.57");
+client.on('connect', function() {
+                    console.log('connected');
+});
+con.connect(function(err) {
+                                          if (err) throw err;
+                                          console.log("Connected!");
+});
 
 const send = (config, fortune) =>{
-    console.log(`Greetings from ${config.firstName} ${config.lastName}: ${fortune}`);
-
-    // write sends to DB
+                                    console.log(`Greetings from ${config.firstName} ${config.lastName}: ${fortune}`);
+                                 var sql = "INSERT INTO fortunes (fortune_sender, fortune, fortune_date) VALUES ('" + config.firstName.replace(/'/g, '\\\'') + " " + config.lastName.replace(/'/g, '\\\'')   + "', '" + fortune.replace(/'/g,'\\\'')+"',CURDATE())";
+                                                  con.query(sql, function (err, result) {
+                                                                                                                if (err) throw err;
+                                                                                                                console.log("1 record inserted into fortunedb:fortunes");
+                                                                                                              });
+                                      var key = config.firstName + " " + config.lastName;
+                                      client.set(key, fortune);
+                                      client.expire(key,60);
 };
 module.exports = {send};
